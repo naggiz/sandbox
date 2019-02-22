349132508223bca46bcdb68ff29f0773842d9eff
Lars H <lars@dr.com>
2019-02-22 20:47:02 +0100

    init

---
+++ install_programs.ps1
@@ -0,0 +1,460 @@
+# Description: Boxstarter / Chocolatey Script
+# Author: Lars H (lars@dr.com)
+# Shoutout to Jessie, ElJefeDSecurIT, TechPreacher and ian-noble for inspiration
+# Created: 25/1-18
+# Last Updated: 15/1-19
+#
+# Install boxstarter:
+# 	. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+#
+# You might need to set: Set-ExecutionPolicy RemoteSigned
+#
+# Run this boxstarter by calling the following from an **elevated** command-prompt:
+# 	Install-BoxstarterPackage -PackageName http://boxstarter.org/package/nr/url?hthttps://gist.githubusercontent.com/hjornevik/6f85a501415f3e429d680c543e51ef00/raw/5743c6446c7cb50303e6d367d50e9a38201f65aa/Ladden-setup.ps1
+# OR
+# 	Install-BoxstarterPackage -PackageName  http://boxstarter.org/package/nr/url?hthttps://gist.githubusercontent.com/hjornevik/6f85a501415f3e429d680c543e51ef00/raw/5743c6446c7cb50303e6d367d50e9a38201f65aa/Ladden-setup.ps1 -DisableReboots
+#
+# Learn more: http://boxstarter.org/Learn/WebLauncher
+
+#---- TEMPORARY ---
+Disable-UAC
+choco feature enable -n allowGlobalConfirmation
+#----           ---
+
+
+#--- Install Chocolatey & Boxstarter ---
+# iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+# choco install -y boxstarter
+
+
+# Initialize reboot log file
+$reboot_log = "C:\installation.rbt"
+if ( -not (Test-Path $reboot_log) ) { New-Item $reboot_log -type file }
+$reboots = Get-Content $reboot_log
+
+
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+
+# Basic setup
+$section = "BasicSetup"
+if(-not ($section -in $reboots)) {
+	Update-ExecutionPolicy Unrestricted
+	Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+	# Taskbar Options if you want to change defaults
+    # Set-TaskbarOptions `
+    # -AlwaysShowIconsOff `
+    # -Size Small `
+    # -Lock
+}
+# Basic setup
+Update-ExecutionPolicy Unrestricted
+
+Set-WindowsExplorerOptions `
+    -EnableShowFullPathInTitleBar `
+	-EnableShowHiddenFilesFoldersDrives `
+	-EnableShowFileExtensions `
+	-DisableOpenFileExplorerToQuickAccess `
+	-DisableShowRecentFilesInQuickAccess `
+	-DisableShowFrequentFoldersInQuickAccess
+
+
+# Update Windows and reboot if necessary
+Install-WindowsUpdate -AcceptEula -GetUpdatesFromMS
+Disable-MicrosoftUpdate
+
+
+#--- Windows Settings ---
+Disable-BingSearch
+Disable-GameBarTips
+
+# don't need remote differential compression if you never intend to pull from network shares...
+Disable-WindowsOptionalFeature -Online -FeatureName MSRDC-Infrastructure -NoRestart
+
+# you don't need Fax & Scan, XPS formats, XPS printing services, or printing to http printers. 
+Disable-WindowsOptionalFeature -Online -FeatureName FaxServicesClientPackage -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName Xps-Foundation-Xps-Viewer -NoRestart
+# Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName Printing-Foundation-InternetPrinting-Client #-NoRestart
+
+# you are not pulling from shares, you should not expose shares...
+Set-service -Name LanmanServer -StartupType Disabled
+#print spooler: Dead
+Set-service -Name Spooler -StartupType Enabled
+# Tablet input: pssh nobody use tablet input. its silly.just write right in onenote
+Set-service -Name TabletInputService -StartupType Disabled
+# Telephony API is tell-a-phony
+Set-service -Name TapiSrv -StartupType Disabled
+#geolocation service : u can't find me.
+Set-service -Name lfsvc -StartupType Disabled
+# ain't no homegroup here.
+Set-service -Name HomeGroupProvider -StartupType Disabled
+# u do not want ur smartcard cert to propagate to the local cache, do you?
+Set-service -Name CertPropsvc -StartupType Disabled
+# who needs branchcache?
+#Set-service -Name PeerDistSvc -StartupType Disabled
+# i don't need to keep links from NTFS file shares across the network - i haz office.
+Set-service -Name TrkWks -StartupType Disabled
+# i don't use iscsi
+Set-service -Name MSISCSI -StartupType Disabled
+# why is SNMPTRAP still on windows 10? i mean, really, who uses SNMP? is it even a real protocol anymore?
+Set-service -Name SNMPTRAP -StartupType Disabled
+# Peer to Peer discovery svcs... Disable
+Set-service -Name PNRPAutoReg -StartupType Disabled
+Set-service -Name p2pimsvc -StartupType Disabled
+Set-service -Name p2psvc -StartupType Disabled
+Set-service -Name PNRPsvc -StartupType Disabled
+# no netbios over tcp/ip. unnecessary. 
+Set-service -Name lmhosts -StartupType Disabled
+# Like plug & play only for network devices. Disable
+Set-service -Name SSDPSRV -StartupType Disabled
+# YOU DO NOT NEED TO PUBLISH FROM THIS DEVICE. Discovery Resource Publication service:
+Set-service -Name FDResPub -StartupType Disabled
+#"Function Discovery host provides a uniform programmatic interface for enumerating system resources". Disable 
+Set-service -Name fdPHost -StartupType Disabled
+#optimize the startup cache...i think. on SSD i don't think it really matters.
+set-service SysMain -StartupType Automatic
+
+# Privacy: Let apps use my advertising ID: Disable
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+    New-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo | Out-Null
+}
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo -Name Enabled -Type DWord -Value 0
+
+# WiFi Sense: HotSpot Sharing: Disable
+If (-Not (Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+    New-Item -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting | Out-Null
+}
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting -Name value -Type DWord -Value 0
+
+# WiFi Sense: Shared HotSpot Auto-Connect: Disable
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots -Name value -Type DWord -Value 0
+
+# Start Menu: Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+# To Restore (Enabled):
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 1
+
+# Change Explorer home screen back to "This PC"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 1
+# Change it back to "Quick Access" (Windows 10 default)
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 2
+
+# Better File Explorer
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+# These make "Quick Access" behave much closer to the old "Favorites"
+# Disable Quick Access: Recent Files
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0
+# Disable Quick Access: Frequent Folders
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0
+# To Restore:
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 1
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 1
+
+# Disable the Lock Screen (the one before password prompt - to prevent dropping the first character)
+If (-Not (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization)) {
+	New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name Personalization | Out-Null
+}
+Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Name NoLockScreen -Type DWord -Value 1
+# To Restore:
+# Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Name NoLockScreen -Type DWord -Value 1
+
+# Lock screen (not sleep) on lid close
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name AwayModeEnabled -Type DWord -Value 1
+# To Restore:
+# Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name AwayModeEnabled -Type DWord -Value 0
+
+# Use the Windows 7-8.1 Style Volume Mixer
+If (-Not (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\MTCUVC")) {
+	New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name MTCUVC | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\MTCUVC" -Name EnableMtcUvc -Type DWord -Value 0
+# To Restore (Windows 10 Style Volume Control):
+# Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\MTCUVC" -Name EnableMtcUvc -Type DWord -Value 1
+
+# Disable Xbox Gamebar
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0
+
+# Turn off People in Taskbar
+# If (-Not (Test-Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
+    # New-Item -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People | Out-Null
+# }
+# Set-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name PeopleBand -Type DWord -Value 0
+
+
+#---LIBRARIES---
+# come on, you know you like em, you use em all the time. might as well make sure they come back each and every time, right?
+# this assumes you set up onedrive. 
+# Move-LibraryDirectory -libraryName "Personal" -newPath $ENV:OneDrive\Documents
+# Move-LibraryDirectory -libraryName "My Pictures" -newPath $ENV:OneDrive\Pictures
+# Move-LibraryDirectory -libraryName "My Video" -newPath $ENV:OneDrive\Videos
+# Move-LibraryDirectory -libraryName "My Music" -newPath $ENV:OneDrive\Music
+ 
+
+#--- Windows Subsystems/Features ---
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+choco install Microsoft-Hyper-V-All -source windowsFeatures
+choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# #--- Tools ---
+choco install TotalCommander -y
+choco install Foxit -y
+choco install chocolateygui -y
+choco install 1password -y
+choco install 7zip -y
+choco install notepadplusplus -y
+choco install putty -y
+choco install filezilla -y
+choco install imgburn -y
+
+
+# # --- DevTools ---
+choco install postman
+choco install soapui
+choco install nodejs.install
+choco install yarn
+cinst python
+cinst pip
+cinst easy.install
+pip install pytest
+pip install math
+Pip install simplejson
+Pip install requests
+pip install libcurl
+Pip install pycurl
+Pip install plotly
+Pip install pyqt # GUI programming
+
+ 
+
+choco install curl -y 
+choco install anaconda3 -y
+choco install ruby-y
+choco install php -y 
+choco install visu
+alstudiocode -y
+choco install visualstudio2017community -y  
+choco install visualstudio2017-workload-manageddesktop -y
+choco install virtualbox -y
+choco install SourceTree -y 
+choco install mongodb -y
+choco install virtualbox 
+choco install docker-for-windows -y 
+choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"' -y
+
+
+
+# # --- Git ---
+Write-Verbose "Install Git"
+choco install poshgit
+Import-Module posh-git
+choco upgrade git
+refreshenv
+git config --global user.name "Lars Hjørnevik"
+git config --global user.email "lars@dr.com"
+git config --global credential.helper wincred
+git config --global --bool pull.rebase true
+
+
+
+# # --- Communication ---
+choco install discord
+choco install skype -y 
+choco install whatsapp -y 
+choco install signal -y
+
+
+
+# # --- Media and office ---
+choco install spotify -y
+choco install itunes -y
+choco install vlc -y
+choco install exiftool -y  # Meta data program
+choco install sharex -y  # Screen capture software, ranked 1
+choco install greenshot -y  # Screen capture software, ranked 2
+choco install IrfanView -y 
+choco install irfanviewplugins -y 
+choco install k-litecodepackfull -y 
+choco install AudaCity -y # Record and edit sounds
+choco install -y flashplayerplugin
+choco install paint.net -y 
+choko install libreoffice-fresh -y
+choco install uplay
+
+
+
+# # --- Apps ---
+choco install brave -y
+choco install googlechrome -y
+choco install tor-browser -y
+choco install Openvpn -y
+choco install thunderbird -y
+choco install evernote -y
+
+
+
+# # --- Games ---
+choco install fs-uae
+choco install freeciv -y
+choco install steam -y
+choco install steamcmd -y  # https://developer.valvesoftware.com/wiki/SteamCMD
+choco install steam-cleaner # Steam Cleaner is a tool that will remove large amounts of data left behind by Steam, Origin, Uplay and GoG.
+choco install epicgameslauncher
+
+
+
+
+# #--- Fonts ---
+choco install inconsolata -y
+choco install sourcecodepro -y
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+
+
+# #--- Uninstall unecessary applications that come with Windows out of the box ---
+# Write-BoxstarterMessage "*** Store Apps Cleanup ***"
+$apps = @(
+    # default Windows 10 apps:
+    "Microsoft.3DBuilder"
+    "Microsoft.Appconnector"
+    "Microsoft.Autodesk"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"
+    "Microsoft.BubbleWitch"
+    #"Microsoft.FreshPaint"
+    "Microsoft.Getstarted"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.Office.OneNote"
+    "Microsoft.OneConnect"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    "Microsoft.Windows.Photos"
+    "Microsoft.WindowsAlarms"
+    #"Microsoft.WindowsCalculator"
+    "Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    "Microsoft.WindowsSoundRecorder"
+    #"Microsoft.WindowsStore"
+    "Microsoft.XboxApp"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.MinecraftUWP"
+    "Microsoft.MicrosoftPowerBIForWindows"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.CommsPhone"
+    "Microsoft.ConnectivityStore"
+    "Microsoft.Messaging"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.BingFoodAndDrink"
+    "Microsoft.BingTravel"
+    "Microsoft.BingHealthAndFitness"
+    "Microsoft.WindowsReadingList"
+    # non-Microsoft:
+    "9E2F88E3.Twitter"
+    "PandoraMediaInc.29680B314EFC2"
+    "Flipboard.Flipboard"
+    "ShazamEntertainmentLtd.Shazam"
+    "king.com.CandyCrushSaga"
+    "king.com.CandyCrushSodaSaga"
+    "king.com.*"
+    "ClearChannelRadioDigital.iHeartRadio"
+    "4DF9E0F8.Netflix"
+    "6Wunderkinder.Wunderlist"
+    "Drawboard.DrawboardPDF"
+    "2FE3CB00.PicsArt-PhotoStudio"
+    "D52A8D61.FarmVille2CountryEscape"
+    "TuneIn.TuneInRadio"
+    "GAMELOFTSA.Asphalt8Airborne"
+    "TheNewYorkTimes.NYTCrossword"
+    "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+    "Facebook.Facebook"
+    "flaregamesGmbH.RoyalRevolt2"
+    "Playtika.CaesarsSlotsFreeCasino"
+    "A278AB0D.MarchofEmpires"
+    "KeeperSecurityInc.Keeper"
+    "ThumbmunkeysLtd.PhototasticCollage"
+    "XINGAG.XING"
+    "89006A2E.AutodeskSketchBook"
+    "D5EA27B7.Duolingo-LearnLanguagesforFree"
+    "46928bounde.EclipseManager"
+    "ActiproSoftwareLLC.562882FEEB491" # next one is for the Code Writer from Actipro Software LLC
+    "CAF9E577.Plex"
+    "*Dropbox*"
+	
+    # # apps which cannot be removed using Remove-AppxPackage
+    # #"Microsoft.BioEnrollment"
+    # #"Microsoft.MicrosoftEdge"
+    # #"Microsoft.Windows.Cortana"
+    # #"Microsoft.WindowsFeedback"
+    # #"Microsoft.XboxGameCallableUI"
+     # #"Microsoft.XboxIdentityProvider"
+    # #"Windows.ContactSupport"
+# )
+
+foreach ($app in $apps) {
+    Write-Output "Trying to remove $app"
+
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+
+    Get-AppXProvisionedPackage -Online |
+        Where-Object DisplayName -EQ $app |
+        Remove-AppxProvisionedPackage -Online -AllUsers
+}
+
+# Uninstall Minecraft
+Get-AppxPackage *Minecraft* | Remove-AppxPackage
+
+# Uninstall Netflix
+Get-AppxPackage *Netflix* | Remove-AppxPackage
+
+# Uninstall McAfee
+Get-AppxPackage *McAfee* | Remove-AppxPackage
+$mcafee = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "McAfee Security" } | select UninstallString
+if ($mcafee) {
+	$mcafee = $mcafee.UninstallString -Replace "C:\Program Files\McAfee\MSC\mcuihost.exe",""
+	Write "Uninstalling McAfee..."
+	start-process "C:\Program Files\McAfee\MSC\mcuihost.exe" -arg "$mcafee" -Wait
+}
+if (Test-PendingReboot) { Invoke-Reboot }
+
+#### Schedule updates to applications with chocolatey
+# schtasks.exe /create /s "localhost" /ru "System" /tn "Update Chocolatey packages" /tr "%ChocolateyInstall%\bin\cup all" /sc DAILY /st 06:00 /F
+
+
+Move-LibraryDirectory "Downloads" "D:\Downloads"
+
+
+# --- Restore Temporary Settings ---
+Update-ExecutionPolicy Unrestricted
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula -GetUpdatesFromMS
+Enable-UAC
+
+
+# --- Rename the Computer ---
+Requires restart, or add the -Restart flag
+$computername = "Ladden"
+if ($env:computername -ne $computername) {
+	Rename-Computer -NewName $computername
+}
+
+if (Test-PendingReboot) { Invoke-Reboot }
